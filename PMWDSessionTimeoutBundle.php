dbefe1be54d90bc7f0eab4f99512029c251e0b13
Michael Piecko <michael.piecko@googlemail.com>
2014-05-30 09:42:56 +0200

    Fixed namespace and doc block

--- PMWDSessionTimeoutBundle.php
+++ PMWDSessionTimeoutBundle.php
@@ -1,9 +1,23 @@
 <?php
 
-namespace PMWD\SessionBundle;
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Michael Piecko <michael.piecko@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PMWD\Bundle\SessionTimeoutBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * PMWDSessionTimeoutBundle.
+ *
+ * @author Michael Piecko <michael.piecko@gmail.com>
+ */
 class PMWDSessionTimeoutBundle extends Bundle
 {
 }
